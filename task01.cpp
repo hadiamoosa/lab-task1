9d26c9af53065bee9e30eb119f92e277daa2ab72
hadiamoosa <hadiamoosa@gmail.com>
2022-12-19 21:19:47 +0500

    my task 1

---
+++ task01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+main (){                                                                                                                          
+
+cout << "my name is hadia";
+cout << endl;
+cout << "i am in section c";  
+}
---
+++ task04.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+main(){
+cout << "   **************  "<<endl;
+cout << "   *            *  "<<endl;
+cout << "   *            *  "<<endl;
+cout << "   **************  "<<endl; }
